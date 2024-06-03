fb9a7bcbd52eee49cfb1dc0fbe07cfadee8a884c
Karush2807 <arushkarnatak1881@gmail.com>
2024-06-04 03:04:26 +0530

    now the user message is shown in the webpage itself without using js

--- dj_chatbot/chatbot/views.py
+++ dj_chatbot/chatbot/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def chatbot(request):
-    return render(request, 'bot_chat.html')
+    user_message = None
+    ai_response = "Hi, I am your AI Chatbot, you can ask me anything."  # Default AI response
+    
+    if request.method == 'POST':
+        user_message = request.POST.get('user_message')
+        # Process the user's message and generate AI response here
+        ai_response = "This is the AI's response to: " + user_message
+
+        
+    return render(request, 'bot_chat.html', {'user_message': user_message, 'ai_response': ai_response})
